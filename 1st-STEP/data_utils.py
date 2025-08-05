06fbb489fc58554b64c945a33a747987ceba2a1b
leanoguerreiro <leanincontact@gmail.com>
2025-08-05 19:52:11 -0400

    Adiciona função para carregar e processar dados do dataset

---
+++ 1st-STEP/data_utils.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+
+
+def load_and_process_data(dataset_dir:str, img_size: tuple[int, int], batch_size:int, validation_split=0.2):
+    """
+    Carrega as imagens do diretório, divide em treino e validação e retorna os datasets
+    
+    :param dataset_dir: 
+    :param img_size: 
+    :param batch_size: 
+    :param validation_split: 
+    :return: 
+    """
+    
+    print("[INFO] Carregando imagens...")
+    
+    # Carrega o conjunto de dados de treino a partir do diretório
+    train_data = tf.keras.utils.image_dataset_from_directory(
+        dataset_dir,
+        validation_split=validation_split,
+        subset="training",
+        seed=123,
+        image_size=img_size,
+        batch_size=batch_size,
+        verbose=True
+    )    
+    
+    validation_data = tf.keras.utils.image_dataset_from_directory(
+        dataset_dir,
+        validation_split=validation_split,
+        subset="validation",
+        seed=123,
+        image_size=img_size,
+        batch_size=batch_size,
+        verbose=True
+    )
+    
+    class_names = train_data.class_names
+    print(f"Módulo de Dados: Encontradas {len(class_names)} classes.")    
+    
+    # Otimiza o carregamento de dados para melhor desempenho
+    autotune = tf.data.AUTOTUNE
+    train_data = train_data.cache().shuffle(1000).prefetch(BUFFER_SIZE=autotune)
+    validation_data = validation_data.cache().prefetch(BUFFER_SIZE=autotune)
+    
+    return train_data, validation_data
