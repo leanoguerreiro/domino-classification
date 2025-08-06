aa78043facb6b0617e6c34073049625f2a806091
leanoguerreiro <leanincontact@gmail.com>
2025-08-05 23:13:21 -0400

    Atualiza configurações do modelo para usar ResNet50 e ajusta número de épocas para 10

--- 1st-STEP-WITH-PYTORCH/config.py
+++ 1st-STEP-WITH-PYTORCH/config.py
@@ -1,11 +1,15 @@
 import torch
 
+MODEL_NAME = "resnet50"
+NUM_CLASSES = 28
+
 K_FOLDS = 5
-NUM_EPOCHS = 20
+RANDOM_SEED = 42
+NUM_EPOCHS = 10
 BATCH_SIZE = 32
 LEARNING_RATE = 0.0001
 
 DATA_DIR = '../data/data-raw'
-MODEL_SAVE_DIR = './models/domino_classifier_final.pth'
+MODEL_SAVE_DIR = f'./models/domino_classifier_{MODEL_NAME}_final.pth'
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
