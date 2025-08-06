5607c545f81735dde5a82830205172d3c32d5b17
leanoguerreiro <leanincontact@gmail.com>
2025-08-05 23:13:25 -0400

    Atualiza configurações do modelo para usar ResNet50 e ajusta número de épocas para 10

---
+++ 1st-STEP-WITH-PYTORCH/dataset.py
@@ -0,0 +1,33 @@
+from torchvision import transforms, datasets
+import config
+
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
+
+IMAGE_SIZE = 224
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(IMAGE_SIZE),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ToTensor(),
+        transforms.Normalize(IMAGENET_MEAN, IMAGENET_STD)
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(IMAGE_SIZE + 32),
+        transforms.CenterCrop(IMAGE_SIZE),
+        transforms.ToTensor(),
+        transforms.Normalize(IMAGENET_MEAN, IMAGENET_STD)
+    ]),
+}
+
+def get_dataset():
+    """
+    Carrega o dataset principal a partir do diretório de dados.
+    """
+    dataset = datasets.ImageFolder(
+        config.DATA_DIR,
+        transform=data_transforms['val']
+    )
+    return dataset
