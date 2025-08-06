b63628a639fbc59419d9bfb668f9afb352cf1bde
leanoguerreiro <leanincontact@gmail.com>
2025-08-05 23:15:28 -0400

    Atualiza tamanho da imagem para 100 pixels

--- 1st-STEP-WITH-PYTORCH/dataset.py
+++ 1st-STEP-WITH-PYTORCH/dataset.py
@@ -4,7 +4,7 @@ import config
 IMAGENET_MEAN = [0.485, 0.456, 0.406]
 IMAGENET_STD = [0.229, 0.224, 0.225]
 
-IMAGE_SIZE = 224
+IMAGE_SIZE = 100
 
 data_transforms = {
     'train': transforms.Compose([
